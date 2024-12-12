a913acc1d1ca9bfc9de408dd96069652f2c5d296
yowawowa <wladizzlaw@gmail.com>
2024-12-12 14:27:41 +0500

    updated

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from simple_bot import Bot
-from scheduled_bot import ScheduledBot
 from longpoll_bot import LongPollBot
-from nlu_longpoll_bot import NLULongPollBot
 
-if __name__ == '__main__':
-    nlu_longpoll_bot = NLULongPollBot()
-    nlu_longpoll_bot.run_long_poll()
+if __name__ == "__main__":
+    longpoll_bot = LongPollBot()
+    longpoll_bot.run_long_poll()
